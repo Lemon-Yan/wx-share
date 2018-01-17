6a4312cea0dbd1de6266609503866240873ed959
FeMoCar <34727834+FeMoCar@users.noreply.github.com>
2018-01-17 19:16:28 +0800

    2018.01.17

---
+++ wx-share/pages/index/index.js
@@ -0,0 +1,32 @@
+//index.js
+Page({
+  data: {
+
+  },
+  onLoad: function () {
+
+  },
+
+  /**
+* 用户点击右上角分享
+*/
+  onShareAppMessage: function (ops) {
+    if (ops.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(ops.target)
+    }
+    return {
+      title: 'xx小程序',
+      path: 'pages/index/index',
+      success: function (res) {
+        // 转发成功
+        console.log("转发成功:" + JSON.stringify(res));
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log("转发失败:" + JSON.stringify(res));
+      }
+    }
+
+  }
+})
